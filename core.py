cb7fc2ff5331ba2c2e84802aa454e11c36dcf0fe
jacobm2000 <maurerja2000@gmail.com>
2025-05-18 13:00:58 -0400

    Added nmap functinality
    Added very basic nmap functionality that scans port 21-443 of target

--- core.py
+++ core.py
@@ -1,5 +1,13 @@
-def run(target):
-    if target!='':
-        return str(target)
-    else:
-        return "no target"
+import nmap
+import threading
+
+def run_scan(target, callback):
+    def task():
+        try:
+            scan = nmap.PortScanner()
+            result = scan.scan(target, '21-443')
+        except Exception as e:
+            result = e
+        callback(result)
+
+    threading.Thread(target=task).start()
--- gui.py
+++ gui.py
@@ -1,5 +1,7 @@
 import tkinter as tk
-from core import run
+from tkinter import scrolledtext
+from core import run_scan
+
 
 #This function0 temporarly allows the output area to be writen to and then disables it after to prevent user tampering
 def append_output(text):
@@ -13,9 +15,12 @@ def clear():
     output.delete('1.0', tk.END)
     output.config(state='disabled')
     
-def start():
-    clear()
-    append_output(run(target_box.get()))
+def handle_scan_result(result):
+    append_output(f'Scan complete: {result}')
+
+def start_scan():
+    target = target_box.get()
+    run_scan(target, handle_scan_result)
 
 
 
@@ -29,9 +34,9 @@ target_label=tk.Label(top_frame,text="Target:")
 target_label.pack(side=tk.LEFT)
 target_box=tk.Entry(top_frame,width=30)
 target_box.pack(side=tk.LEFT,padx=10)
-start_btn=tk.Button(top_frame,text="Start",command=start)
+start_btn=tk.Button(top_frame,text="Start",command=start_scan)
 start_btn.pack(side=tk.LEFT, padx=10)
-output=tk.scrolledtext.ScrolledText(root,wrap=tk.WORD)
+output=scrolledtext.ScrolledText(root,wrap=tk.WORD)
 output.config(state='disabled')
 output.pack(pady=20)
 root.mainloop()
